a15ed550804158648b1fe3a0cd9952d504dc3b12
Nimit_Shah <nimitshah099@gmail.com>
2025-06-28 21:55:53 +0530

    All api work done

--- src/main/java/com/ia/server/student/service/StudentService.java
+++ src/main/java/com/ia/server/student/service/StudentService.java
@@ -1,19 +1,16 @@
 package com.ia.server.student.service;
 
 import com.ia.server.base.model.Student;
-import com.ia.server.base.repository.BaseStudentRepository;
 import com.ia.server.base.service.BaseStudentService;
 import com.ia.server.student.repository.StudentRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
-import java.util.Optional;
 
 
 @Service
@@ -26,19 +23,20 @@ public class StudentService extends BaseStudentService {
 
 
     @Transactional
-    public Student saveOrUpdateStudent(HttpServletRequest request, Student student) {
+    public void saveOrUpdateStudent(HttpServletRequest request, Student student) {
         try {
             String ipAddress = request.getHeader("X-Forwarded-For");
 
-            ipAddress = (ipAddress != null && !ipAddress.isEmpty() ? ipAddress.split(",")[0] : request.getRemoteAddr());
+//            ipAddress = (ipAddress != null && !ipAddress.isEmpty() ? ipAddress.split(",")[0] : request.getRemoteAddr());
 
-            student.setIp(ipAddress);
-            RestTemplate restTemplate = restTemplateBuilder.build();
-            String url = "https://ipinfo.io/" + ipAddress + "/json/";
-            Map<String, Object> mapOfIP = restTemplate.getForObject(url, Map.class);
-            student.setLocation(mapOfIP.get("city") + "," + mapOfIP.get("region"));
-
-            return studentRepository.save(student);
+            if (ipAddress != (null)) {
+                RestTemplate restTemplate = restTemplateBuilder.build();
+                String url = "https://ipinfo.io/" + ipAddress + "/json/";
+                Map<String, Object> mapOfIP = (Map<String, Object>) restTemplate.getForObject(url, Map.class).putIfAbsent("city", null);
+                student.setIp(ipAddress);
+                student.setLocation(mapOfIP.get("city") + "," + mapOfIP.get("region"));
+            }
+            studentRepository.save(student);
         } catch (Exception e) {
             System.out.println("saveOrUpdateStudent : " + e);
             throw new RuntimeException(e);
